3ac1cbee68a01ce932cd4e5bc9ca7fbb8dfe23cb
Rui Gonçalves <figurinhapt@gmail.com>
2023-07-05 17:46:26 +0100

    Polymorphism and Abstraction
    Adding polymorphism and abstraction to the code

--- Assets/Classes/Cube.cs
+++ Assets/Classes/Cube.cs
@@ -4,12 +4,44 @@ using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
-    public virtual void CreateCube(GameObject cube, Color color, Vector3 position)
+    private bool used = false;
+
+    public virtual void EditCube(GameObject cube, Color color, Vector3 position)
     {
         //change color
         cube.GetComponent<MeshRenderer>().material.SetColor("_Color", color);
 
-        Instantiate(cube, position, cube.transform.rotation);
-
+        //change position
+        cube.transform.position = position;
     }
+
+    // POLYMORPHISM
+    public virtual void WackyStuff(int sizeMultiplyer, GameObject cube) //increase size of cube
+    {
+        if (used)
+        {
+            cube.transform.localScale /= sizeMultiplyer;
+            used = false;
+        } 
+        else
+        {
+            cube.transform.localScale *= sizeMultiplyer;
+            used = true;
+        }
+        
+    }
+
+    // POLYMORPHISM
+    public virtual void WackyStuff(GameObject cube) //change cube to random color
+    {
+        cube.GetComponent<MeshRenderer>().material.SetColor("_Color", Random.ColorHSV());
+    }
+
+    // POLYMORPHISM
+    public virtual bool WackyStuff(GameObject cube, bool visible) //make cube dissapear and re-appear
+    {
+        cube.SetActive(!visible);
+        return !visible;
+    }
+
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 
 public class GameManager : Cube //INHERITANCE
 {
+    private GameObject[] cubes = new GameObject[3];
+    private bool visible = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        CreateCube(GameObject.CreatePrimitive(PrimitiveType.Cube), Color.red, new Vector3(0, 0, 0));
-        CreateCube(GameObject.CreatePrimitive(PrimitiveType.Cube), Color.green, new Vector3(2, 0, 0));
-        CreateCube(GameObject.CreatePrimitive(PrimitiveType.Cube), Color.black, new Vector3(-2, 0, 0));
+        AddCubes(); //ABSTRACTION
+
+        InitCubes(); //ABSTRACTION
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W)) // POLYMORPHISM
+        {
+            WackyStuff(2, cubes[0]);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E)) // POLYMORPHISM
+        {
+            WackyStuff(cubes[1]);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q)) // POLYMORPHISM
+        {
+            visible = WackyStuff(cubes[2], visible);
+        }
+    }
+
+    private void AddCubes()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            cubes[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        }
+    }
+
+    private void InitCubes()
+    {
+        EditCube(cubes[0], Color.red, new Vector3(0, 0, 0));
+        EditCube(cubes[1], Color.green, new Vector3(2, 0, 0));
+        EditCube(cubes[2], Color.black, new Vector3(-2, 0, 0));
     }
 }
